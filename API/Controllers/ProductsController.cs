e2514d68de11a1cfef6b158990aa00396dae8f0e
mohamed-shokrani <mohamedshokrani55@gmail.com>
2023-06-23 18:03:22 +0300

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructre.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _Context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _Context = context;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var all = await _Context.products.ToListAsync();
+        return Ok(all);
+    }
+}
---
+++ Infrastructre/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructre.Data;
+
+public class AppDbContext :DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+	{
+
+	}
+
+	public DbSet<Product> products { get; set; }
+
+}
